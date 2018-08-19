243db46a1782699732083a81a55e41e1dad0cc49
vesonet <42510215+vesonet@users.noreply.github.com>
2018-08-19 11:15:03 +0100

    Update youtube-auto-delete-watch-later.js

--- youtube-auto-delete-watch-later.js
+++ youtube-auto-delete-watch-later.js
@@ -1,13 +1,22 @@
 
+function wait(ms, cb) {
+  var waitDateOne = new Date();
+  while ((new Date()) - waitDateOne <= ms) {
+    //Nothing
+  }
+  if (cb) {
+    eval(cb);
+  }
+}
 function clf(e,i){ 
-	console.log('click');
+	console.log('removed');
 	e.click(); 
 } 
 
 function c(){  
 	d() 
 	f() 
-	setInterval(f,6000) 
+	setInterval(f,50000) 
 }
 
 function f(){
@@ -18,11 +27,9 @@ function f(){
 function d(){
 	var el = document.getElementsByClassName('pl-video-edit-remove');  
 	for (var i=0;i<el.length; i++) {  
-	 clf(el[i],i)   
+	 wait(500,clf(el[i],i));  
 	}
 }
 
 c() 
-setInterval(d, 15000);
-
-
+setInterval(d, 60000); 
